5d390672772403a66fe325739f8b86184571cc75
Simon Steeldune <nighttagle@gmail.com>
2022-01-21 13:54:20 +0100

    Changed some graph settings, and added the last graph

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from numpy.random import default_rng
 
 class Wiener():
     def __init__(self, wiener_sample, scale=None, start=None,
-                 end=None):  # Accepts an array with two rows, one with a Wiener process, the other the time samples.
+                 end=None, h=1.0,
+                 K=1.0):  # Accepts an array with two rows, one with a Wiener process, the other the time samples.
         if scale is None:
             self.min_scale = wiener_sample[0, 1] - wiener_sample[0, 0]
         else:
@@ -23,14 +24,16 @@ class Wiener():
         self.sample = wiener_sample[1]
         self.axis_x = wiener_sample[0]
         self.solved = np.zeros(self.max_samples)
+        self.h = h
+        self.K = K
 
     @classmethod
-    def generate_new(cls, scale, end, start=0.0):
+    def generate_new(cls, scale, end, start=0.0, h=1.0, K=1.0):
         axis_x = np.arange(start, end, scale)
         random_table = rng.normal(size=len(axis_x))
         scale_variance = np.sqrt(scale)
         sample = np.cumsum(random_table * scale_variance + start)
-        return cls(np.array([axis_x, sample]), scale=scale, start=start, end=end)
+        return cls(np.array([axis_x, sample]), scale=scale, start=start, end=end, h=h, K=K)
 
     @classmethod
     def generate_upscale(cls, wiener):
@@ -46,28 +49,42 @@ class Wiener():
         return cls(upscale_wiener)
 
     @classmethod
-    def generate_downscale(cls, wiener):
+    def generate_downscale(cls, wiener, h=1.0, K=1.0):
         downscale_wiener = np.zeros((2, len(wiener[0]) // 2))
         for i in range(len(downscale_wiener[0])):
             downscale_wiener[0, i] = wiener[0, i * 2]
             downscale_wiener[1, i] = wiener[1, i * 2]
-        return cls(downscale_wiener)
+        return cls(downscale_wiener, h=h, K=K)
 
     def apply_euler(self, f_func, g_func, ini_pos=0.0):
         self.solved[0] = ini_pos
         for i in range(self.max_samples - 1):
             x = self.solved[i]
-            self.solved[i + 1] = x + f_func(x, self.axis_x[i]) * self.min_scale + g_func(x, self.axis_x[i]) * (
-                    self.sample[i + 1] - self.sample[i])
+            self.solved[i + 1] = x + f_func(x, self.axis_x[i], h=self.h, K=self.K) * self.min_scale + g_func(x,
+                                                                                                             self.axis_x[
+                                                                                                                 i],
+                                                                                                             h=self.h,
+                                                                                                             K=self.K) * (
+                                         self.sample[i + 1] - self.sample[i])
         return self.solved.copy()
 
     def apply_milstein(self, f_func, g_func, g_func_der, ini_pos=0.0):
         self.solved[0] = ini_pos
         for i in range(self.max_samples - 1):
             x = self.solved[i]
-            self.solved[i + 1] = x + f_func(x, self.axis_x[i]) * self.min_scale + g_func(x, self.axis_x[i]) * (
-                    self.sample[i + 1] - self.sample[i]) + 0.5 * g_func(x, self.axis_x[i]) * g_func_der(x, self.axis_x[
-                i]) * (np.power(self.sample[i + 1] - self.sample[i], 2.0) - self.min_scale)
+            self.solved[i + 1] = x + f_func(x, self.axis_x[i], h=self.h, K=self.K) * self.min_scale + g_func(x,
+                                                                                                             self.axis_x[
+                                                                                                                 i],
+                                                                                                             h=self.h,
+                                                                                                             K=self.K) * (
+                                         self.sample[i + 1] - self.sample[i]) + 0.5 * g_func(x, self.axis_x[i],
+                                                                                             h=self.h,
+                                                                                             K=self.K) * g_func_der(x,
+                                                                                                                    self.axis_x[
+                                                                                                                        i],
+                                                                                                                    h=self.h,
+                                                                                                                    K=self.K) * (
+                                         np.power(self.sample[i + 1] - self.sample[i], 2.0) - self.min_scale)
         return self.solved.copy()
 
     def apply_function(self, function):
@@ -94,10 +111,12 @@ def func(x, b=0.3):
     # return x
     return 0
 
+
 def f_func(x, t, h=1.0, K=1.0):
     z = x % (h / 2.0)
-    # return K * (h - 4 * z)
-    return 0
+    return K * (h - 4 * z)
+    # return 0
+
 
 def g_func(x, t, h=1.0, K=1.0):
     z = x % (h / 2.0)
@@ -110,9 +129,9 @@ def g_func_der(x, t, h=1.0, K=1.0):
     return K * (h / np.sqrt(2.0) - 2.0 * np.sqrt(2.0) * z) / denom
 
 
-def g_func_alt(x, t, h=1.0, k=2.0):
+def g_func_alt(x, t, h=1.0, K=2.0):
     z = x % (h / 2.0)
-    return z * (h - 2 * z) * k
+    return z * (h - 2 * z) * K
 
 
 def g_func_der_alt(x, t, h=1.0, K=2.0):
@@ -121,28 +140,35 @@ def g_func_der_alt(x, t, h=1.0, K=2.0):
 
 
 if __name__ == '__main__':
-    depth = 1.0
-    rng = default_rng()
-    nr_samples = 100
-    time_end = 10
-    time_scale = 0.001
-    nr_scales = 7
-    nr_solvers = 7
-    mfd = int(2 ** (nr_scales - 1))
-    ini_point = depth / 4
-    nr_histo_frames = 5
-    E_or_M = False
+    '''These are the parameters by which the code can be altered, after the E_or_M parameter, 
+    all code should function on its own'''
+    depth = 1.0  # This is h, the scale of the simulation
+    diffusion = 1.0  # This is K, the speed of the diffusion
+    nr_samples = 1000  # The number of particles that will be simulated
+    time_end = 1  # t starts at 0, and ends at time_end
+    time_scale = 0.001  # This is dt, the time-scale of the most accurate simulation
+    nr_scales = 7  # This is the number of additional particles will be simulated at lower sampling of the Wiener
+    # process. Each subsequent simulation will double the time-scale.
+    ini_point = depth / 4  # Where the particles start in z
+    nr_histo_frames = 5  # The number of histogram frames will be rendered of the particle distribution. They will be
+    # made with regular intervals of time in between.
+    E_or_M = False  # Is the simulation running the Euler (True) or Milstein scheme (False)
 
     scale_origin = 0
+    nr_solvers = nr_scales
+    mfd = int(2 ** (nr_scales - 1))
+    rng = default_rng()
 
     wiener_list = [None for x in range(nr_scales)]
-    wiener_list[scale_origin] = [Wiener.generate_new(time_scale, time_end) for i in range(nr_samples)]
+    wiener_list[scale_origin] = [Wiener.generate_new(time_scale, time_end, h=depth, K=diffusion) for i in
+                                 range(nr_samples)]
 
-    for i in range(scale_origin):
-        wiener_list[scale_origin - i - 1] = [Wiener.generate_upscale(wiener.export()) for wiener in
-                                             wiener_list[scale_origin - i]]
+    # for i in range(scale_origin):
+    #     wiener_list[scale_origin - i - 1] = [Wiener.generate_upscale(wiener.export()) for wiener in
+    #                                          wiener_list[scale_origin - i]]
     for i in range(scale_origin + 1, nr_scales):
-        wiener_list[i] = [Wiener.generate_downscale(wiener.export()) for wiener in wiener_list[i - 1]]
+        wiener_list[i] = [Wiener.generate_downscale(wiener.export(), h=depth, K=diffusion) for wiener in
+                          wiener_list[i - 1]]
 
     fig, ax = plt.subplots()
     fig.set_size_inches(5.2, 5 / 4 * 3)
@@ -162,7 +188,8 @@ if __name__ == '__main__':
             if E_or_M:
                 solved_list[i][j] = wiener_list[i][j].apply_euler(f_func, g_func, ini_pos=ini_point)
             else:
-                solved_list[i][j] = wiener_list[i][j].apply_milstein(f_func, g_func_alt, g_func_der_alt, ini_pos=ini_point)
+                solved_list[i][j] = wiener_list[i][j].apply_milstein(f_func, g_func, g_func_der,
+                                                                     ini_pos=ini_point)
 
             if i == 0:
                 wiener_list[i][j].plot_solved(ax, label='Solved {}'.format(i))
@@ -177,7 +204,7 @@ if __name__ == '__main__':
     ax.set_xlabel('time (t)')
     ax.set_ylabel('position (z)')
     ax.set_title('Position of Particles over time in {} scheme'.format(('Milstein', 'Euler')[E_or_M]))
-    for i in np.arange(0.0, depth+0.1, depth*0.5):
+    for i in np.arange(0.0, depth + 0.1, depth * 0.5):
         ax.axhline(i, linestyle='--', color='gray')
 
     # ax.legend()
@@ -194,7 +221,7 @@ if __name__ == '__main__':
     ax3.set_xlabel('time (t)')
     ax3.set_ylabel('position (z)')
     ax3.set_title('A. One process with different sampling from the Wiener Process')
-    for i in np.arange(0.0, depth+0.1, depth*0.5):
+    for i in np.arange(0.0, depth + 0.1, depth * 0.5):
         ax3.axhline(i, linestyle='--', color='gray')
 
     ax2 = fig.add_subplot(2, 1, 2)
@@ -228,7 +255,7 @@ if __name__ == '__main__':
         plt.ylim(0.0, 4.5)
 
     figWeak = plt.figure(figsize=(5.5, 5.3 / 4 * 3), dpi=300)
-    ax5 = figWeak.add_subplot(1,1,1)
+    ax5 = figWeak.add_subplot(1, 1, 1)
     figWeak.subplots_adjust(hspace=0.5)
 
     error_mean = np.zeros((nr_solvers, len(solved_list[0][0]) // mfd))
@@ -241,7 +268,7 @@ if __name__ == '__main__':
         scale_time = wiener_list[scale][0].min_scale
         particle_ends = np.array([solved_list[scale][i][-1] for i in range(nr_samples)])
         particle_ends_1 = 0.0 < particle_ends
-        particle_ends_2 = particle_ends < depth*0.5
+        particle_ends_2 = particle_ends < depth * 0.5
         particle_esc[scale] = nr_samples - np.sum(particle_ends_1 * particle_ends_2)
         if scale == 0:
             ori_mean = scale_mean.copy()
@@ -250,7 +277,7 @@ if __name__ == '__main__':
                 error_mean[scale - (nr_scales - nr_solvers), k // mfd] = np.abs(
                     ori_mean[k] - scale_mean[k // 2 ** scale])
     dt_variances = [error_mean[i][-1] for i in range(1, nr_scales)]
-    particle_esc = particle_esc/nr_samples * 100
+    particle_esc = particle_esc / nr_samples * 100
     ax5.loglog(delta_ts[1:], dt_variances, label='Sample Error')
     ax5.loglog(x_test, y_test, label='Error fit O(dt^1/2)')
     ax5.loglog(x_test, x_test, label='Error for O(dt^1)')
@@ -263,13 +290,12 @@ if __name__ == '__main__':
     ax5.set_ylabel('Error'.format(time_end))
 
     figEscape = plt.figure(figsize=(5.5, 5.3 / 4 * 3), dpi=300)
-    ax6 = figEscape.add_subplot(1,1,1)
+    ax6 = figEscape.add_subplot(1, 1, 1)
     ax6.plot(delta_ts, particle_esc)
     ax6.grid()
     ax6.set_title('Escaped Particles in the {} scheme'.format(('Milstein', 'Euler')[E_or_M]), fontsize=15)
     ax6.set_xlabel('time-step (dt)')
     ax6.set_ylabel('frequency (%)')
     ax6.set_xscale('log')
-
 
     plt.show()
